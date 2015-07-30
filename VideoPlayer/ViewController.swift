0e7b561b7c7d4c2522315432136efcaa7b0028e9
daniel <danielnanuk@gmail.com>
2015-07-30 14:22:11 +0800

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by 周梦 on 7/30/15.
+//  Copyright (c) 2015 野狗部落. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
