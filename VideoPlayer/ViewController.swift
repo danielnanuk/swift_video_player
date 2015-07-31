41043c96ffc8d049eefc93d72968fd4eddc8e050
daniel <danielnanuk@gmail.com>
2015-07-31 15:22:55 +0800

    swift_video_player

--- VideoPlayer/ViewController.swift
+++ VideoPlayer/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
+import MediaPlayer
+
 class ViewController: UIViewController {
 
+    var mediaVideoPath = NSURL(fileURLWithPath: "http://7ktofw.com1.z0.glb.clouddn.com/Intro.mp4")
+
+    var mediaPlayer = MPMoviePlayerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +24,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playVideo(){
+        mediaPlayer = MPMoviePlayerController(contentURL: mediaVideoPath)
+        mediaPlayer.prepareToPlay()
+        mediaPlayer.view.frame = CGRectMake(0, 0, 320, 320)
+        view.addSubview(mediaPlayer.view)
+        mediaPlayer.play()
     }
 
 
